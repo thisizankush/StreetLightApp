4e7b12e13dd459c74797b708b8f6d22c3b5c2017
thisizankushk <ankushgautam510@gmail.com>
2022-10-03 17:10:53 +0530

    Initial commit

---
+++ app/src/main/java/com/example/streetlightapp/loginmodule/LoginActivity.kt
@@ -0,0 +1,88 @@
+package com.example.streetlightapp.loginmodule
+
+import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.example.streetlightapp.R
+import com.example.streetlightapp.databinding.LoginBinding
+import com.example.streetlightapp.utils.Constants
+import com.example.streetlightapp.utils.SessionManagementActivity
+
+class LoginActivity : AppCompatActivity() {
+    lateinit var binding: LoginBinding
+    private lateinit var viewModel: LoginViewModel
+    private lateinit var sessionManagementActivity: SessionManagementActivity
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        sessionManagementActivity = SessionManagementActivity(this)
+        if (!sessionManagementActivity.isLoggedIn) {
+            binding = DataBindingUtil.setContentView(this, R.layout.login)
+            viewModel = ViewModelProvider(this)[LoginViewModel::class.java]
+            initView()
+        } else {
+
+        }
+    }
+
+    private fun initView() {
+        binding.loginLayout.setOnClickListener {
+            binding.loginLayout.isEnabled = false
+            binding.progressbar.visibility = View.VISIBLE
+            viewModel.getUserLogin(
+                binding.edtEmail.text.toString(),
+                binding.edtPass.text.toString()
+            ).observe(this) {
+                if (TextUtils.isEmpty(binding.edtEmail.text)) {
+                    binding.apply {
+                        edtEmail.error = "field required"
+                        edtEmail.requestFocus()
+                        loginLayout.isEnabled = true
+                        progressbar.visibility = View.GONE
+                    }
+
+                } else if (TextUtils.isEmpty(binding.edtPass.text)) {
+                    binding.apply {
+                        edtPass.error = "field required"
+                        edtPass.requestFocus()
+                        loginLayout.isEnabled = true
+                        progressbar.visibility = View.GONE
+                    }
+
+                } else {
+                    if (it != null && it.statusCode == 200) {
+
+                        Toast.makeText(this, "logged in", Toast.LENGTH_SHORT)
+                            .show()
+
+
+                        sessionManagementActivity.insertData(this, Constants.API_TOKEN, it.data?.token)
+                        sessionManagementActivity.createLoginSession(
+                            it.data?.token,
+                            it.data?.user!!.name
+                        )
+                        Thread.sleep(1200)
+                        Log.d("TAG", "initView: ${it.data?.token}")
+                        binding.progressbar.visibility = View.GONE
+//                        val intent = Intent(this, ChooserActivity::class.java)
+//                        startActivity(intent)
+                        finish()
+                        binding.loginLayout.isEnabled = true
+
+                    } else {
+                        binding.progressbar.visibility = View.GONE
+                        binding.loginLayout.isEnabled = true
+                        Toast.makeText(this, "Incorrect Details", Toast.LENGTH_SHORT).show()
+
+                    }
+                }
+
+            }
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/streetlightapp/loginmodule/LoginRepo.kt
@@ -0,0 +1,38 @@
+package com.example.streetlightapp.loginmodule
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.streetlightapp.network.RetrofitInstance
+import retrofit2.Call
+import retrofit2.Response
+import javax.security.auth.callback.Callback
+
+class LoginRepo {
+    fun userLogin(email:String,pass:String): LiveData<LoginUserModel> {
+        val mutableLiveDetailData: MutableLiveData<LoginUserModel> = MutableLiveData()
+        val call = RetrofitInstance.create().userLogin(email,pass)
+        call?.enqueue(object : Callback, retrofit2.Callback<LoginUserModel?> {
+            override fun onResponse(
+                call: Call<LoginUserModel?>,
+                response: Response<LoginUserModel?>,
+            ) {
+                try {
+                    if (response.code() == 200) {
+                        mutableLiveDetailData.postValue(response.body())
+                    } else {
+                        mutableLiveDetailData.postValue(null)
+                    }
+                } catch (e: Exception) {
+                    mutableLiveDetailData.postValue(null)
+                }
+            }
+
+            override fun onFailure(call: Call<LoginUserModel?>, t: Throwable) {
+                Log.d("TAG", "onFailure: ")
+            }
+
+        })
+        return mutableLiveDetailData
+    }
+}
